57c0627e26977380a44ee64d25b3d9e97164a107
Alejandro Junqui <ajunquit@gmail.com>
2025-09-11 10:18:53 -0500

    refactor

--- apps/frontend/src/Morning.Value.Web.Site/Books/Models/BookCreateViewModel.cs
+++ apps/frontend/src/Morning.Value.Web.Site/Books/Models/BookCreateViewModel.cs
@@ -5,13 +5,13 @@ namespace Morning.Value.Web.Site.Books.Models
     public class BookCreateViewModel
     {
         [Required, StringLength(150)]
-        public string Title { get; set; } = "";
+        public string Title { get; set; } = string.Empty;
 
         [Required, StringLength(120)]
-        public string Author { get; set; } = "";
+        public string Author { get; set; } = string.Empty;
 
         [Required, StringLength(60)]
-        public string Genre { get; set; } = "";
+        public string Genre { get; set; } = string.Empty;
 
         [Range(0, int.MaxValue, ErrorMessage = "Debe ser >= 0")]
         public int AvailableCopies { get; set; }
